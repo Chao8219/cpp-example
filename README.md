98d85206d7d9e37c07b50cba1ef460fa89c0994c
Chao <zichao@gwu.edu>
2018-08-09 13:03:53 -0400

    comments and README updated

--- README.md
+++ README.md
@@ -2,8 +2,39 @@
 
 This file is an example of c++
 
-Currently contains header file declaration and source file implemetion.
+# Contents
+
+1. [Vector Usage](#vector-usage)
+
+2. [Header Guards](#header-guards)
+
+3. [To-do List](#to-do-list)
+
+# Vector Usage
+
+In order to use the std::vector, one should `include <vector>`, and maybe use `use namespace std` if needed.
+
+
+[back to top](#contents)
+
+# Header Guards
+
+The header guards is commonly used in header files to avoid duplicate definitions.
+
+```
+#ifndef EXAMPLE_NAME_H
+#define EXAMPLE_NAME_H
+
+// codes here
+
+#endif
+```
+
+[back to top](#contents)
 
 # To-do List
 
 - [ ] Update README about `vector<Robot> robotVec(1,robot)`
+- [x] Make table of contents
+
+[back to top](#contents)
--- include/coffee_robot.hpp
+++ include/coffee_robot.hpp
@@ -12,15 +12,15 @@ class CoffeeRobot
     public:
         CoffeeRobot(); /**< Default constructor. */
         CoffeeRobot(string& robot_name); /**< Overload the constructor with robot_name */
-        virtual ~CoffeeRobot();
+        virtual ~CoffeeRobot(); /**< Default deconstructor. */
         
-        int robot_numero;
-        double position_x;
-        double position_y;
-        string robot_name;
+        int robot_numero; /**< The number of robot. */
+        double position_x; /**< The x position of robot. */
+        double position_y; /**< The y position of robot. */
+        string robot_name; /**< The name of robot */
         
-        void print_welcome();
-        void print_position();
+        void print_welcome(); /**< Printing out welcome message. */
+        void print_position(); /**< Printing out the current position of robot. */
 };
 
 #endif
